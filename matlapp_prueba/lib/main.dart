e8f5554eef692eeada824fae2f25dbe894943cff
Billy Salazar <billysalazar@MBP-de-Billy.lan>
2023-03-08 11:36:56 -0300

    splash screen y avances home

--- matlapp_prueba/lib/main.dart
+++ matlapp_prueba/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,64 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Matlapp',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromRGBO(88, 29, 125, 1)),
+      ),
+      home: const PaginaInicial(),
+    );
+  }
+}
+
+class PaginaInicial extends StatelessWidget {
+  const PaginaInicial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //Quita barra de estado
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.bottom]);
+
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Color.fromRGBO(87, 41, 122, 1),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Container(
+            // color: Color.fromRGBO(245, 223, 187, 1),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(245, 223, 187, 1),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset(
+                      'lib/assets/images/bienvenida/icono_entrada.png'),
+                ),
+                Container(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset(
+                      'lib/assets/images/bienvenida/isologotipo_app.png'),
+                ),
+                Container(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset(
+                      'lib/assets/images/bienvenida/aprende_jugando_tarjeta.png'),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
